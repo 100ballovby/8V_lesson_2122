ea270d6a0aa94a8e9b74cbaa6fd121d87377d1b2
demidraksin <greatraksin@icloud.com>
2021-09-01 15:16:11 +0300

    Initial commit🐍

---
+++ 00_lesson_0109/01_variables.py
@@ -0,0 +1,24 @@
+x = 5 * 7 + 32 / 11 * 3
+print(type(x))  # функция type() возвращает тип того, что передано ей в скобках
+y = x
+print(x == y)
+
+y = y * 0
+print(x, y)
+
+square_five = 5 ** 2  # возведение в степень
+print(square_five)
+
+puppets = 2
+print('У меня', puppets, 'щенка')
+puppets = 2.5
+print('У меня', puppets, 'щенка')
+
+a = 429
+print(type(a))
+a = str(a)
+print(type(a))
+b = '56789'
+b = int(b)
+print(b / 2)
+
